de75e98740f3c809118eac6c1a4142ba09cc1e97
afialapis <devel@afialapis.com>
2022-03-09 00:46:37 +0100

    ..

--- configs/xeira.js
+++ configs/xeira.js
@@ -1,12 +1,20 @@
 module.exports= {
-  // Probably offering alternatives some day
-  linter: 'eslint', // 'none'
-  transpiler: 'babel', // 'none'
-  minifier: 'uglify', // 'none'
-  bundler: 'rollup', // 'webpack', 'esbuild'
 
   product: 'package', // 'app',
   target: 'node', // 'browser', 'all',
+
+  source_index: './src/index.js', // 'index.js'
+
+  // Probably offering alternatives some day
+  linter: 'eslint', // 'none'
+  
+  transpile_folder: 'none', // './lib
+  transpiler: 'babel', // 'none'
+  minifier: 'uglify', // 'none'
+
+  bundle_folder: './dist', // './build', './lib'
+  bundler: 'rollup', // 'webpack', 'esbuild'
+
   react: false,
 
   // This can be previously tried to be guessed (/packages or .pnpm-workspace.yaml),
