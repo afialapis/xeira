dc1b7436cf9dc4b2c6765f45e2b94d8324802da4
afialapis <devel@afialapis.com>
2022-04-23 02:37:42 +0200

    versioning ++

--- src/scripts/version/index.js
+++ src/scripts/version/index.js
@@ -13,4 +13,48 @@ process.on('unhandledRejection', err => {
   throw err;
 });
 
-const execSync = require('child_process').execSync;
+(async () => {
+
+  const {getXeiraConfigObj} = require('../../config/xeira')
+  const versioning = require('./versioning')
+
+  const pkgPath= process.env.PWD;
+  
+  // get args
+  const args = process.argv.slice(2);
+
+  let versionType= '', filterPattern= ''
+  if (args.length>=1) {
+    versionType = args[0] || ''
+
+    if (args.length>1) {
+      if (args[1]=='-filter') {
+        filterPattern= args[2]
+      } else {
+        throw Error('Invalid filtering parameters')
+      }
+    }
+
+  } else {
+    throw Error('Invalid number of parameters')
+  }
+
+  // get xeira config
+  const xeiraConfig = getXeiraConfigObj(pkgPath)
+
+  // do the versioning
+  await versioning(pkgPath, xeiraConfig, versionType, filterPattern)
+  
+})().catch((error) => {
+  process.exitCode = 1;
+  const msg= `[xeira] version: error ${error.message}. 
+  Usage:
+  
+  > npx xeira version patch|minor|major|0.1.2
+
+  If you are in a monorepo:
+
+  > npx xeira version patch|minor|major|0.1.2 [-filter <pattern>]
+  `
+  console.error(msg)
+});
