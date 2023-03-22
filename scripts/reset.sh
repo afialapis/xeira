2163b187c32cddceb348945bd1e7ac9fd691654a
afialapis <devel@afialapis.com>
2023-03-22 01:07:28 +0100

    reorg tests

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -2,12 +2,12 @@
 # 
 reset_and_prepare_pkg() {
   PREVDIR=$PWD
-  cd tests/packages/$1
+  cd tests/$1
   npm run --silent x-prepare
   cd $PREVDIR
 }
 
-for TEST_PATH in tests/packages/*
+for TEST_PATH in tests/*
 do
   TEST=$(basename "$TEST_PATH")
   echo "Preparing $TEST"
--- scripts/test.sh
+++ scripts/test.sh
@@ -2,14 +2,14 @@
 # 
 test_pkg() {
   PREVDIR=$PWD
-  cd tests/packages/$1
+  cd tests/$1
   echo ""
   echo "Testing $1..."
   npm run --silent x-run
   cd $PREVDIR
 }
 
-for TEST_PATH in tests/packages/*
+for TEST_PATH in tests/*
 do
   TEST=$(basename "$TEST_PATH")
   test_pkg $TEST
